0fcd97f800aeedd1d17a4c88d937ceb1b9050e68
pjlee-deg <pjlee@degenkolb.com>
2024-07-01 08:50:12 -0700

    Update conf.py

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -2,9 +2,9 @@
 
 # -- Project information
 
-project = 'Lumache'
-copyright = '2021, Graziella'
-author = 'Graziella'
+project = 'PyCSI'
+copyright = '2021, Degenkolb Engineers'
+author = 'Peter Lee'
 
 import os
 import sys
